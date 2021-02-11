ba218a50c586cec229f5bb8f936d18aebc41170c
Tetsu Matsu <mtst.tty556@gmail.com>
2021-02-11 17:56:30 +0900

    関数定義のファイル作成

---
+++ work/web/index2.php
@@ -0,0 +1,17 @@
+<?php
+
+require('../app/functions.php');
+
+$name = 'Taro <script>alert(1);</script>';
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>PHP Practice</title>
+</head>
+<body>
+  <p>Hello, <?= h($name) ?>!</p>
+</body>
+</html>
