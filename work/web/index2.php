eb173e621504e1b98db8e7a99e240afee25745a2
Tetsu Matsu <mtst.tty556@gmail.com>
2021-02-23 14:27:39 +0900

    form add

--- work/web/index2.php
+++ work/web/index2.php
@@ -8,6 +8,7 @@ include('../app/_parts/_header.php');
 
 <form action="result.php" method="get">
   <input type='text' name="message">
+  <input type='text' name="usename">
   <button>send</button>
 </form>
 <?php
--- work/web/result.php
+++ work/web/result.php
@@ -2,12 +2,15 @@
 
 require('../app/functions.php');
 
+$message = filter_input(INPUT_GET, 'message');
+$usename = filter_input(INPUT_GET, 'usename');
+
 include('../app/_parts/_header.php');
 
 ?>
 
-<p><?= h($message); ?></p>
-<p><a href="index.php">Go back</a></p>
+<p><?= h($message); ?> by <?= h($usename); ?></p>
+<p><a href="index2.php">Go back</a></p>
 <?php
 
 include('../app/_parts/_footer.php');
