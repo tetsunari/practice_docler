11eb0d07e7995e61c05c261246aac801bf55e394
Tetsu Matsu <mtst.tty556@gmail.com>
2021-02-26 07:48:45 +0900

    multipleと配列の受け取り方

--- work/web/result.php
+++ work/web/result.php
@@ -2,13 +2,14 @@
 
 require('../app/functions.php');
 
-$color = filter_input(INPUT_GET, 'color');
+$colors = filter_input(INPUT_GET, 'colors', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
+$colors = empty($colors) ? 'None selected' : implode(',', $colors);
 
 include('../app/_parts/_header.php');
 
 ?>
 
-  <p><?= h($color); ?></p>
+  <p><?= h($colors); ?></p>
   <p><a href="index2.php">Go back</a></p>
 
 <?php
