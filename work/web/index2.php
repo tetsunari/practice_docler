63c644c610df5b4048482ff10dc2351ebabb95bb
Tetsu Matsu <mtst.tty556@gmail.com>
2021-02-25 15:44:40 +0900

    trim & 入力文字列のチェック

--- work/web/index2.php
+++ work/web/index2.php
@@ -6,11 +6,11 @@ include('../app/_parts/_header.php');
 
 ?>
 
-<form action="result.php" method="get">
-  <input type='text' name="message">
-  <input type='text' name="usename">
-  <button>send</button>
-</form>
+  <form action="result.php" method="get">
+    <input type="text" name="message">
+    <button>Send</button>
+  </form>
+
 <?php
 
 include('../app/_parts/_footer.php');
--- work/web/result.php
+++ work/web/result.php
@@ -2,15 +2,16 @@
 
 require('../app/functions.php');
 
-$message = filter_input(INPUT_GET, 'message');
-$usename = filter_input(INPUT_GET, 'usename');
+$message = trim(filter_input(INPUT_GET, 'message'));
+$message = $message !== '' ? $message : '...';
 
 include('../app/_parts/_header.php');
 
 ?>
 
-<p><?= h($message); ?> by <?= h($usename); ?></p>
-<p><a href="index2.php">Go back</a></p>
+  <p><?= h($message); ?></p>
+  <p><a href="index2.php">Go back</a></p>
+
 <?php
 
 include('../app/_parts/_footer.php');
