75593bdfd4dc1963a0812c874c182faa3a3ab6b9
Tetsu Matsu <mtst.tty556@gmail.com>
2021-02-26 07:40:23 +0900

    select

--- work/web/index2.php
+++ work/web/index2.php
@@ -7,7 +7,11 @@ include('../app/_parts/_header.php');
 ?>
 
   <form action="result.php" method="get">
-    <textarea name="message"></textarea>
+    <select name="color">
+      <option value="orange">Orange</option>
+      <option value="pink">Pink</option>
+      <option value="gold">Gold</option>
+    </select>
     <button>Send</button>
   </form>
 
--- work/web/result.php
+++ work/web/result.php
@@ -2,14 +2,13 @@
 
 require('../app/functions.php');
 
-$message = trim(filter_input(INPUT_GET, 'message'));
-$message = $message !== '' ? $message : '...';
+$color = filter_input(INPUT_GET, 'color');
 
 include('../app/_parts/_header.php');
 
 ?>
 
-  <p><?= nl2br(h($message)); ?></p>
+  <p><?= h($color); ?></p>
   <p><a href="index2.php">Go back</a></p>
 
 <?php
