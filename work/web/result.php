c3df56e709e5f9b9fb835010489345b45a8d5447
Tetsu Matsu <mtst.tty556@gmail.com>
2021-03-07 20:05:12 +0900

    radiobox

--- work/web/result.php
+++ work/web/result.php
@@ -2,14 +2,16 @@
 
 require('../app/functions.php');
 
-$colors = filter_input(INPUT_GET, 'colors', FILTER_DEFAULT, FILTER_REQUIRE_ARRAY);
-$colors = empty($colors) ? 'None selected' : implode(',', $colors);
+// $color = filter_input(INPUT_GET, 'color');
+// $color = isset($color) ? $color : 'None selected';
+// $color = $color ?? 'None selected';  //上二つと同じ動き
+$color = filter_input(INPUT_GET, 'color') ?? 'None selected';
 
 include('../app/_parts/_header.php');
 
 ?>
 
-  <p><?= h($colors); ?></p>
+  <p><?= h($color); ?></p>
   <p><a href="index2.php">Go back</a></p>
 
 <?php
