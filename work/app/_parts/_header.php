6ed42878f3ad57a83e98175ec2e846f916e0dddf
Tetsu Matsu <mtst.tty556@gmail.com>
2021-04-06 07:06:37 +0900

    掲示板準備

--- work/app/_parts/_header.php
+++ work/app/_parts/_header.php
@@ -1,12 +1,7 @@
-<?php
-
-$color = $_SESSION['color'] ?? 'transparent';
-
-?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
   <meta charset="utf-8">
   <title>PHP Practice</title>
 </head>
-<body style="background-color: <?= h($color); ?>";>
+<body>
--- work/app/functions.php
+++ work/app/functions.php
@@ -1,7 +1,5 @@
 <?php
 
-session_start();
-
 function h($str)
 {
   return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
