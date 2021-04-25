ba8b7cecc64a743d2b6c035248484662e8db40d7
tsunari <mtst.tty556@gmail.com>
2021-04-25 19:31:55 +0900

    post messageの書き込みと表示

---
+++ work/web/result.php
@@ -0,0 +1,22 @@
+<?php
+
+require('../app/functions.php');
+
+$message = trim(filter_input(INPUT_POST, 'message'));
+$message = $message !== '' ? $message : '...';
+
+$filename = '../app/messages.txt';
+$fp = fopen($filename, 'a');
+fwrite($fp, $message . "\n");
+fclose($fp);
+
+include('../app/_parts/_header.php');
+
+?>
+
+  <p>Message added!</p>
+  <p><a href="index2.php">Go back</a></p>
+
+<?php
+
+include('../app/_parts/_footer.php');
