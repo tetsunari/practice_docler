b7a64aee87da4b84b4bf977c2034312da80b6a68
tsunari <mtst.tty556@gmail.com>
2021-04-26 23:46:48 +0900

    二重投稿防止

--- work/web/result.php
+++ work/web/result.php
@@ -2,24 +2,12 @@
 
 require('../app/functions.php');
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $message = trim(filter_input(INPUT_POST, 'message'));
-  $message = $message !== '' ? $message : '...';
-  
-  $filename = '../app/messages.txt';
-  $fp = fopen($filename, 'a');
-  fwrite($fp, $message . "\n");
-  fclose($fp);
-} else {
-  exit('Invalid Request');
-}
-
 include('../app/_parts/_header.php');
 
 ?>
 
   <p>Message added!</p>
-  <p><a href="index.php">Go back</a></p>
+  <p><a href="index2.php">Go back</a></p>
 
 <?php
 
